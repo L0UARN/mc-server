5a53435a666ad083c054d09a13fa27debd1e02f8
Louarn RICHARD <louarn.richard@proton.me>
2023-01-14 23:35:43 +0100

    Auto save

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 git pull
 
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.2.3/win_args.txt %*
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.2.3/win_args.txt %* --nogui
 
 git add .
 git commit -m "Auto save"
